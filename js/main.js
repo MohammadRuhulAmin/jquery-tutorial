eebb31261d142922d3ebd0ea2c75d1513e674fd9
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-02-27 13:01:01 +0600

    mouse double click event

--- js/main.js
+++ js/main.js
@@ -1,24 +1,10 @@
 $(document).ready(function(){
-   $('#btn-show').hide();
-   $('#btn-hide').click(function(){
-        $('#description').hide();
-        $('#btn-show').show();
-   });
-
-   $('#btn-show').click(()=>{
-        $('#description').show();
-        $('#btn-hide').show();
-        $('#btn-show').hide();
-   });
-   $('#hideMe').click(function(){
-       $('*').hide();
-   });
-   $('#mySelf').click(function(){
-    $(this).hide();
-   });
-   $('#listHide').click(function(){
-        $('ul li:nth-child(1)').hide();
-   });  
+  $('#btn_1').click(function(){
+    $('#para_1').hide();
+  });
+  $('#btn_2').dblclick(function(){
+    $('#para_1').show();
+  });
 });
 
 
