52567aabecd79bc1d60d64fd591ed9163acf548b
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-02-28 14:44:55 +0600

    adding hide function to jquery chaining

--- js/main_6.js
+++ js/main_6.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#btn_1').click(function(){
-        $('#div_1').css("font-size","25px").slideUp(2000).slideDown(2000);
+        $('#div_1').css("font-size","25px").slideUp(2000).slideDown(2000).hide(1000);
     });
 
 
