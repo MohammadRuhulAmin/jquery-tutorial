2b03a9187ea6479ce7211c3959f4e6f1c7945893
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-02-28 23:42:01 +0600

    ajax in jquery introduction

---
+++ js/main_10.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#ajax_btn').click(function(){
+        $('#ajax_para').load("index_5.html");
+    });
+
+
+});
--- js/main_8.js
+++ js/main_8.js
@@ -2,12 +2,17 @@ $(document).ready(function(){
     $('#btn_1').click(()=>{
         $('#para_1').append("Md Ruhul Amin");
     });
+
+
     $('#btn_2').click(()=>{
         $('#para_1').prepend("Ruhul");
     });
+
+
     $('#before').click(function(){
         $('#para_2').before("hi ruhul amin");
     });
+
     $('#after').click(function(){
         $('#para_2').after('Hi ruhul amin');
     });
@@ -20,19 +25,23 @@ $(document).ready(function(){
         $('#div_3').remove();
     });
 
+
     $('#style').click(function(){
         $("#my_style_text").addClass("ruhul about");
-
     });
+
 
     $('#style_2').click(function(){
         $('#my_style_text_toggle').toggleClass("ruhul about");
     });
+
+
     $('#btn_dim').click(function(){
+        
         $('#dimention').css({
             "font-size":"25px",
             "color":"red",
-            "background":"green"   
+            "background":"green"
         }).width(500).height(50);
     });
 
