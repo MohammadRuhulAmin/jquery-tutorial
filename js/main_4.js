0124e980c28436154904227a87a3c766378e6b40
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-02-28 10:48:42 +0600

    X-modal created

--- js/main_4.js
+++ js/main_4.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
     $('#btn_2').click(function(){
-        $('#custom_modal').hide(2000);
+        $('#custom_modal').hide();
     });
     $('#btn_1').click(function(){
-        $('#custom_modal').show(1000);
+        $('#custom_modal').show();
     });
 
     $('#btn_3').click(function(){
         $('#custom_modal').hide();
-
     });
 
 
@@ -18,6 +17,10 @@ $(document).ready(function(){
     });
     $('#btn_x').click(function(){
         $('#message_1').hide();
+    });
+
+    $('#X-machine').click(function(){
+        $('#X-modal').fadeToggle();
     });
 
 
