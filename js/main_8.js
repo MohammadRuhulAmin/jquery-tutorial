1ad679854664cdc05d954fac74243d90a40ef086
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-02-28 17:59:31 +0600

    prepend text using prepend() action

--- js/main_8.js
+++ js/main_8.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     $('#btn_1').click(()=>{
         $('#para_1').append("Md Ruhul Amin");
     });
+    $('#btn_2').click(()=>{
+        $('#para_1').prepend("Ruhul");
+    });
 
 
 });
